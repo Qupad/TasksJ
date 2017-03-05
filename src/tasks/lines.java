210ff30ef9187ce737e733afb6f6d92fe77be14e
Qupad <capzed@gmail.com>
2017-03-06 00:43:10 +0300

    Initial commit

--- src/tasks/lines.java
+++ src/tasks/lines.java
@@ -6,9 +6,16 @@ package tasks;
 public class lines {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++)
+            System.out.print(" "+8);
+        System.out.println();
+        for (int i = 0; i < 10; i++) {
+            System.out.print(8);
+                for (int c = 0; c < 19;c++) {
+                System.out.print(" ");
+                }
             System.out.println(8);
+        }
         for (int a = 0; a < 10; a++)
             System.out.print(" "+8);
-
     }
 }
