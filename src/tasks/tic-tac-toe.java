05ee3ac1a3256f81714b30fbdd310c87250bf131
Qupad <capzed@gmail.com>
2017-03-06 20:09:51 +0300

    Initial commit DO SOME DO A FCK ETERNITY thx VLAD for dat

--- src/tasks/tic-tac-toe.java
+++ src/tasks/tic-tac-toe.java
@@ -24,12 +24,9 @@ import java.util.*;
             }
 
             void printBoard() {
-                while (true) {
-                    System.out.println(this.board[0][0] + " " + this.board[0][1] + " " + this.board[0][2]);
-                    System.out.println(this.board[1][0] + " " + this.board[1][1] + " " + this.board[1][2]);
-                    System.out.println(this.board[2][0] + " " + this.board[2][1] + " " + this.board[2][2]);
-                    break;
-                }
+                    System.out.println(this.board[0][0] + "  " + this.board[0][1] + "  " + this.board[0][2]);
+                    System.out.println(this.board[1][0] + "  " + this.board[1][1] + "  " + this.board[1][2]);
+                    System.out.println(this.board[2][0] + "  " + this.board[2][1] + "  " + this.board[2][2]);
             }
             boolean checkLoser(){
 
