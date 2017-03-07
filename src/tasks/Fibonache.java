b66df4f295b31da64b150f264ee8ba136ff0c2e6
Qupad <capzed@gmail.com>
2017-03-07 16:37:58 +0300

    Initial commit DO SOME DO A FCK ETERNITY thx VLAD for dat now with X and O                                                                                    /no way NOW WITH RESET !!111 AND AND ITS RLLY FCK INFINITY SHIT I DID FCK FOR OMG

---
+++ src/tasks/Fibonache.java
@@ -0,0 +1,10 @@
+package tasks;
+
+/**
+ * Created by Maniak on 06.03.2017.
+ */
+public class Fibonache {
+    public static void main(String[] args) {
+        System.out.println((char) 79);
+    }
+}
